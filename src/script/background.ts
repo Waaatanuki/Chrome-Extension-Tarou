98a56c0c482170aecd68ab387f9836f85b8312bb
Waaatanuki <watanukipublic@gmail.com>
2023-08-07 09:46:19 +0800

    fix: 修复计数错误

--- src/script/background.ts
+++ src/script/background.ts
@@ -144,7 +144,7 @@ import { Raid_EternitySand, Raid_GoldBrick, targetRaid } from '~/constants/raid'
         treasure.key === '73_1' && hitRaid!.ring1++
         treasure.key === '73_2' && hitRaid!.ring2++
         treasure.key === '73_3' && hitRaid!.ring3++
-        hitRaid!.lastBlueChestCount = treasure.key === '17_20004' ? 0 : hitRaid!.lastBlueChestCount++
+        hitRaid!.lastBlueChestCount = treasure.key === '17_20004' ? 0 : ++hitRaid!.lastBlueChestCount
       }
       if (treasure.key === '17_20004') {
         data.goldBrick = treasure.box
