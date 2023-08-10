b8f9170f88a7a60d32de41a9dff96dc746f7cefe
Waaatanuki <watanukipublic@gmail.com>
2023-08-10 10:38:33 +0800

    feat: 沙漏统计增加导入导出功能

--- src/views/options/main.vue
+++ src/views/options/main.vue
@@ -1,16 +1,79 @@
 <script setup lang="ts">
+import dayjs from 'dayjs'
 import RaidCard from './components/RaidCard.vue'
 import { eternitySandData } from '~/logic/storage'
+import type { RaidInfo } from '~/logic/types'
+
+const filesList = ref([])
+
+function handleUploadChange(uploadFile: any) {
+  const selectedFile = uploadFile.raw
+
+  const reader = new FileReader()
+  reader.readAsText(selectedFile)
+  reader.onload = function () {
+    const dataSet = JSON.parse(reader.result as string)
+    dataSet.forEach((item: RaidInfo) => {
+      const hit = eternitySandData.value.find(raid => raid.quest_id === item.quest_id)
+      if (hit) {
+        hit.total! += item.total ?? 0
+        hit.blueChest! += item.blueChest ?? 0
+        hit.eternitySand! += item.eternitySand ?? 0
+      }
+    })
+    ElMessage.success('导入成功')
+  }
+}
+
+function handleExport() {
+  const exportData = eternitySandData.value.reduce((pre, cur) => {
+    pre.push({
+      quest_id: cur.quest_id,
+      quest_name_en: cur.quest_name_en,
+      quest_name_jp: cur.quest_name_jp,
+      total: cur.total,
+      blueChest: cur.blueChest,
+      eternitySand: cur.eternitySand,
+    })
+    return pre
+  }, [] as any[])
+
+  const data = JSON.stringify(exportData, null, 2)
+  const timeStr = dayjs().format('YYYY-MM-DD')
+  const content = new Blob([data])
+  const urlObject = window.URL || window.webkitURL || window
+  const url = urlObject.createObjectURL(content)
+  const el = document.createElement('a')
+  el.href = url
+  el.download = `沙漏统计数据 ${timeStr}.json`
+  el.click()
+  urlObject.revokeObjectURL(url)
+  ElMessage.success('导出成功')
+}
 </script>
 
 <template>
   <main font-sans>
     <div sticky left-0 right-0 top-0 z-999 h-10 fc bg-violet text-base>
       沙漏掉落统计
-      <div
-        dark:i-carbon-moon i-carbon-sun absolute right-4 icon-btn
-        @click="toggleDark()"
-      />
+      <div absolute right-4 fc gap-2>
+        <div fc btn text-xs @click="handleExport">
+          <div mr-1 i-carbon:document-export />
+          导出
+        </div>
+        <el-upload v-model:file-list="filesList" :on-change="handleUploadChange" :show-file-list="false" :limit="1" :auto-upload="false" accept=".json">
+          <template #trigger>
+            <div fc btn text-xs @click="filesList = []">
+              <div mr-1 i-carbon:document-import />
+              导入
+            </div>
+          </template>
+        </el-upload>
+        <div
+          dark:i-carbon-moon i-carbon-sun icon-btn
+          @click="toggleDark()"
+        />
+      </div>
     </div>
     <RaidCard :data="eternitySandData.filter(i => i.visiable)" />
     <el-collapse>
