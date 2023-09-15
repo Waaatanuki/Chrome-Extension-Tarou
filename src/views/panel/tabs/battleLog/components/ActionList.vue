da7cfd1147b53c6bdba4e1d61ab1fa18c50f6ab8
Waaatanuki <watanukipublic@gmail.com>
2023-09-15 10:58:56 +0800

    feat(战斗日志): 优化操作列表样式

--- src/views/panel/tabs/battleLog/components/ActionList.vue
+++ src/views/panel/tabs/battleLog/components/ActionList.vue
@@ -24,50 +24,40 @@ function getNpcImg(action: Action) {
 <template>
   <el-card v-if="battleRecord" min-w-400px>
     <el-scrollbar height="520px">
-      <el-descriptions :column="1" border>
-        <el-descriptions-item v-for="list, idx in battleRecord.actionQueue" :key="idx" label-class-name="action-list-label">
-          <template #label>
-            <div relative p-10px h-full>
-              <div mb-5px>
-                {{ `第${idx + 1}回合` }}
-              </div>
-              <div fc gap-2px>
-                <el-check-tag v-for="index in 4" :key="index" label="G" :checked="!!list.guard_status[index - 1]?.is_guard_status">
-                  G
-                </el-check-tag>
-              </div>
-              <div absolute left-5px top-5px>
-                <el-tag :type="list.special_skill_flag ? 'danger' : 'success'" effect="dark" size="small">
-                  {{ list.special_skill_flag ? 'OFF' : 'ON' }}
-                </el-tag>
-              </div>
-              <div absolute right-5px top-5px text-sm>
-                {{ list.bossHpPercent.toString().split('.')[0] }}
-              </div>
-            </div>
-          </template>
-
-          <div flex flex-wrap items-center justify-start gap-10px>
-            <div v-for="action, i in list.acitonList" :key="i" fc>
-              <img h-50px :src="getImg(action)">
-              <template v-if="action.aim_num">
-                <div i-carbon:arrow-right text-xl />
-                <img h-50px :src="getNpcImg(action)">
-              </template>
-            </div>
+      <el-card
+        v-for="list, idx in battleRecord.actionQueue" :key="idx"
+        :body-style="{ padding: '0px', display: 'flex' }"
+        shadow="hover"
+      >
+        <div relative w-200px fc shrink-0 flex-col gap-10px p-10px class="bg-#f5f7fa dark:bg-#262727">
+          <div text-base font-bold>
+            {{ `第${idx + 1}回合` }}
           </div>
-        </el-descriptions-item>
-      </el-descriptions>
+          <div fc gap-2px>
+            <el-check-tag v-for="index in 4" :key="index" label="G" :checked="!!list.guard_status[index - 1]?.is_guard_status">
+              G
+            </el-check-tag>
+          </div>
+          <div absolute left-5px top-5px>
+            <el-tag :type="list.special_skill_flag ? 'danger' : 'success'" effect="dark" size="small">
+              {{ list.special_skill_flag ? 'OFF' : 'ON' }}
+            </el-tag>
+          </div>
+          <div absolute right-5px top-5px text-sm>
+            {{ list.bossHpPercent.toString().split('.')[0] }}
+          </div>
+        </div>
+
+        <div flex flex-wrap items-center justify-start gap-10px p-10px>
+          <div v-for="action, i in list.acitonList" :key="i" fc>
+            <img h-50px :src="getImg(action)">
+            <template v-if="action.aim_num">
+              <div i-carbon:arrow-right w-20px text-xl />
+              <img h-50px :src="getNpcImg(action)">
+            </template>
+          </div>
+        </div>
+      </el-card>
     </el-scrollbar>
   </el-card>
 </template>
-
-<style>
-.el-descriptions__label.el-descriptions__cell.is-bordered-label.action-list-label {
-  width: 200px;
-  font-size: large;
-  text-align: center;
-  padding: 0px;
-  font-weight: 500;
-}
-</style>
