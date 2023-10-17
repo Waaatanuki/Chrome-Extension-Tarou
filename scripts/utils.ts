15fda70841c98347c1e37d479aa5bd4a658b61c9
Waaatanuki <watanukipublic@gmail.com>
2023-10-17 16:36:02 +0800

    chore: 模式改为ES modules

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,3 +1,7 @@
-import { resolve } from 'node:path'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export const r = (...args: string[]) => resolve(__dirname, '..', ...args)
