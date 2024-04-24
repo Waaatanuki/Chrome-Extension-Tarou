b0beed2f0dbeeb1bbdc498ee764e17ba71d8c0eb
Waaatanuki <watanukipublic@gmail.com>
2024-04-24 18:53:02 +0800

    feat: 增加用于调试的页面

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const data = ref({
+  questId: '305381',
+  questName: 'ムゲンＨＬ',
+  isBlueBox: true,
+  isBlueTreasure: true,
+  targetItemKey: '10_215',
+  questImage: '305381',
+  questType: '0',
+  targetItemCount: 3,
+  total: 478,
+  blueChest: 370,
+  lastDropCount: 6,
+  lastDropTake: 18,
+})
+</script>
+
+<template>
+  <div fc flex-wrap>
+    <QuestCard :data="data" />
+  </div>
+</template>
