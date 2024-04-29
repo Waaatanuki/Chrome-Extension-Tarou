f584b83bb59900335d0548d54c1c9aea340ca0be
Waaatanuki <watanukipublic@gmail.com>
2024-04-29 16:36:52 +0800

    feat(战斗日志): 优化召唤显示逻辑

--- src/views/debugger/tabs/battleLog/components/Summon.vue
+++ src/views/debugger/tabs/battleLog/components/Summon.vue
@@ -1,28 +1,32 @@
 <script setup lang="ts">
+import type { Summon } from 'requestData'
 import { storeToRefs } from 'pinia'
 
 const battleLogStore = useBattleLogStore()
 const { summonInfo } = storeToRefs(battleLogStore)
+
+const summonList = computed(() => {
+  const res = summonInfo.value?.summon.reduce<Summon[]>((pre, cur) => {
+    cur.id && pre.push(cur)
+    return pre
+  }, []) ?? []
+
+  if (summonInfo.value?.supporter.id)
+    res.push(summonInfo.value.supporter)
+
+  return res
+})
 </script>
 
 <template>
-  <ElCard v-if="summonInfo" :body-style="{ padding: '10px' }">
+  <ElCard v-if="summonList.length > 0" :body-style="{ padding: '10px' }">
     <div fc gap-5px>
-      <div v-for="idx in 5" :key="idx" class="party_weapon_wrapper">
-        <img v-if="summonInfo.summon[idx - 1]?.image_id" w-full :src="getAssetImg('summon', summonInfo.summon[idx - 1]?.image_id)">
-        <div v-if="summonInfo.summon[idx - 1]?.image_id && Number(summonInfo.summon[idx - 1]?.recast) !== 0" absolute h-full w-full fc>
+      <div v-for="summon, idx in summonList" :key="idx" class="party_weapon_wrapper">
+        <img w-full :src="getAssetImg('summon', summon.image_id)">
+        <div v-if=" Number(summon.recast) !== 0" absolute h-full w-full fc>
           <div absolute h-full w-full bg-slate-900 opacity-50 />
           <div z-999 h-30px w-30px fc border-3 border-red-700 rounded-full text-lg text-white>
-            {{ Number(summonInfo.summon[idx - 1]?.recast) > 100 ? '∞' : summonInfo.summon[idx - 1]?.recast }}
-          </div>
-        </div>
-      </div>
-      <div v-if="summonInfo.supporter?.id" class="party_weapon_wrapper">
-        <img w-full :src="getAssetImg('summon', summonInfo.supporter.image_id)">
-        <div v-if=" Number(summonInfo.supporter.recast) !== 0" absolute h-full w-full fc>
-          <div absolute h-full w-full bg-slate-900 opacity-50 />
-          <div z-999 h-30px w-30px fc border-3 border-red-700 rounded-full text-lg text-white>
-            {{ Number(summonInfo.supporter.recast) > 100 ? '∞' : summonInfo.supporter.recast }}
+            {{ Number(summon.recast) > 100 ? '∞' : summon.recast }}
           </div>
         </div>
       </div>
