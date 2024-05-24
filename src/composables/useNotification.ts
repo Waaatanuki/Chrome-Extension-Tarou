92e159c0b73b6e332171d0dd8b7e5d7910c7f3e5
Waaatanuki <watanukipublic@gmail.com>
2024-05-24 15:47:42 +0800

    feat: 增加调用弹窗时的失败处理

--- src/composables/useNotification.ts
+++ src/composables/useNotification.ts
@@ -4,6 +4,14 @@ export function createNotification(message: string, iconUrl?: string) {
     message,
     type: 'basic',
     title: '通知',
-    silent: false,
+  }, () => {
+    if (chrome.runtime.lastError) {
+      chrome.notifications.create({
+        iconUrl: '/assets/icon-48.png',
+        message,
+        type: 'basic',
+        title: '通知',
+      })
+    }
   })
 }
