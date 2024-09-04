f1a3eb86914e8cf7b9df989a45869dfc8648d839
Waaatanuki <watanukipublic@gmail.com>
2024-09-04 11:23:44 +0800

    chore

--- src/views/debugger/tabs/battleLog/components/Summon.vue
+++ src/views/debugger/tabs/battleLog/components/Summon.vue
@@ -23,7 +23,7 @@ const summonList = computed(() => {
   <ElCard v-if="summonList.length > 0" :body-style="{ padding: '10px' }">
     <div fc gap-5px>
       <div v-for="summon, idx in summonList" :key="idx" class="party_weapon_wrapper">
-        <img w-full :src="getAssetImg('summon', summon.image_id)">
+        <img h-full w-full :src="getAssetImg('summon', summon.image_id)">
         <div v-if=" Number(summon.recast) !== 0" absolute h-full w-full fc>
           <div absolute h-full w-full bg-slate-900 opacity-50 />
           <div z-999 h-30px w-30px fc border-3 border-red-700 rounded-full text-lg text-white>
