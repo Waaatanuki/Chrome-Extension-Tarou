62cfeb78a2ec9951c1963d6b1876ad1ed0f9376e
Waaatanuki <watanukipublic@gmail.com>
2025-04-13 12:55:25 +0800

    feat: 调整首页链接展示

--- src/views/debugger/tabs/dashboard/components/Contact.vue
+++ src/views/debugger/tabs/dashboard/components/Contact.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
 defineEmits(['open'])
+
+function jumpTo(url: string) {
+  window.open(url, '_blank')
+}
 </script>
 
 <template>
   <div fc flex-col gap-4>
-    <div i-carbon-sun dark:i-carbon-moon text-10 icon-btn @click="toggleDark()" />
+    <div i-carbon-home text-10 icon-btn @click="jumpTo('https://gbf.pub')" />
+    <div i-carbon-sun dark:i-carbon-moon text-5 icon-btn @click="toggleDark()" />
     <div i-carbon:debug text-5 icon-btn @click="$emit('open', 'debug')" />
-    <a
-      i-carbon:logo-github text-5 hover:text-amber
-      rel="noreferrer"
-      href="https://github.com/Waaatanuki/Chrome-Extension-Tarou"
-      target="_blank" title="GitHub"
-    />
+    <div i-carbon:logo-github text-5 icon-btn @click="jumpTo('https://github.com/Waaatanuki/Chrome-Extension-Tarou')" />
     <div i-carbon:settings text-5 icon-btn @click="$emit('open', 'setting')" />
   </div>
 </template>
