158b791eb259e7187884c9d88a0843ed715ba814
Waaatanuki <watanukipublic@gmail.com>
2025-06-06 22:13:50 +0800

    chore: 调整文件目录

--- src/views/sidePanel/views/dashboard/index.vue
+++ src/views/sidePanel/views/dashboard/index.vue
@@ -2,7 +2,7 @@
 import RecoveryItemTable from './components/RecoveryItemTable.vue'
 import Status from './components/Status.vue'
 import StoneCount from './components/StoneCount.vue'
-import Treasureraid from './components/Treasureraid.vue'
+import Treasureraid from './event/Treasureraid.vue'
 </script>
 
 <template>
