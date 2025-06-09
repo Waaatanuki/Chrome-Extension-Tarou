1f2de0ed91b251b79aae382d1efe0f89e4b9f18d
Waaatanuki <watanukipublic@gmail.com>
2025-06-09 17:13:03 +0800

    feat: ws解析改在sw执行

--- src/background/index.ts
+++ src/background/index.ts
@@ -316,6 +316,16 @@ import { battleInfo, battleMemo, deckList, eventList, isSidePanelOpened, localNp
       chrome.windows.remove(obWindowId.value).catch(() => {})
   })
 
+  chrome.debugger.onEvent.addListener((source, method, params: any) => {
+    if (method === 'Network.webSocketFrameReceived') {
+      const payloadData: string = params.response?.payloadData || ''
+      if (payloadData.substring(0, 2) === '42') {
+      // console.log(JSON.parse(payloadData.substring(2))[1].bossUpdate?.param?.boss1_condition)
+        handleWsPayloadJson(JSON.parse(payloadData.substring(2))[1])
+      }
+    }
+  })
+
   chrome.windows.onRemoved.addListener((windowId) => {
     if (windowId === obWindowId.value) {
       obWindowId.value = 0
--- src/views/debugger/main.vue
+++ src/views/debugger/main.vue
@@ -9,16 +9,6 @@ import EvokerPage from './tabs/evoker/index.vue'
 import MarkedUser from './tabs/markedUser/index.vue'
 import Party from './tabs/party/index.vue'
 
-chrome.debugger.onEvent.addListener((source, method, params: any) => {
-  if (method === 'Network.webSocketFrameReceived') {
-    const payloadData: string = params.response?.payloadData || ''
-    if (payloadData.substring(0, 2) === '42') {
-      // console.log(JSON.parse(payloadData.substring(2))[1].bossUpdate?.param?.boss1_condition)
-      handleWsPayloadJson(JSON.parse(payloadData.substring(2))[1])
-    }
-  }
-})
-
 chrome.windows.onBoundsChanged.addListener((windowInfo) => {
   if (obWindowId.value !== windowInfo.id)
     return
