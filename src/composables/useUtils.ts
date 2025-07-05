a179546cf3755673822fea0e880497b90f6cfdeb
Waaatanuki <watanukipublic@gmail.com>
2025-07-05 08:50:33 +0800

    chore

--- src/composables/useUtils.ts
+++ src/composables/useUtils.ts
@@ -15,7 +15,7 @@ export function openPopupWindow(key: string) {
   const windowSize: Record<string, { height: number, width: number }> = {
     SupportSummon: { height: 400, width: 600 },
     ArtifactRule: { height: 700, width: 700 },
-    RecoveryItem: { height: 800, width: 900 },
+    RecoveryItem: { height: 1000, width: 900 },
   }
 
   chrome.windows.create({ url: `src/views/popup/main.html?${key}`, type: 'popup', ...(windowSize[key] ?? { height: 800, width: 800 }) })
