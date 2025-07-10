732bd2293f803375233aa087db92e6a3aa059639
Waaatanuki <watanukipublic@gmail.com>
2025-07-10 20:23:38 +0800

    feat: 调整队伍信息样式

--- src/views/sidePanel/views/party/index.vue
+++ src/views/sidePanel/views/party/index.vue
@@ -9,17 +9,14 @@ const deck = computed(() => deckList.value[0])
 </script>
 
 <template>
-  <div h-10 flex items-center rounded bg-neutral-8 px-4 text-base>
-    <TheButton @click="openPopupWindow('BuildCompare')">
-      配置对比
-    </TheButton>
-  </div>
-
-  <div v-if="deck" my-10px flex flex-col gap-10px>
+  <div v-if="deck" flex flex-col gap-10px>
     <Npc :leader="deck.leader" :npcs="deck.npcs" />
     <Weapon :weapons="deck.weapons" />
     <Summon :summons="deck.summons" />
     <Effect :effects="deck.effects" />
   </div>
   <el-result v-else icon="info" sub-title="进入编成界面读取队伍信息" />
+  <TheButton mt-10px w-full @click="openPopupWindow('BuildCompare')">
+    配置对比
+  </TheButton>
 </template>
