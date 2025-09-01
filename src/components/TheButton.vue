eb763de6494cb0db9034ca5aa9f864ffd2022f74
Waaatanuki <watanukipublic@gmail.com>
2025-09-01 18:32:50 +0800

    feat: 优化组件封装

--- src/components/TheButton.vue
+++ src/components/TheButton.vue
@@ -1,22 +1,16 @@
 <script setup lang="ts">
+import type { ButtonProps } from 'element-plus'
 import { Icon } from '@iconify/vue'
 
-withDefaults(defineProps<{
-  size?: '' | 'default' | 'small' | 'large'
-  type?: '' | 'default' | 'text' | 'success' | 'warning' | 'info' | 'primary' | 'danger'
-  color?: string
+type MyButtonProps = Partial<ButtonProps> & {
   icon?: string
-  loading?: boolean
-}>(), {
-  size: 'small',
-  type: 'primary',
-  color: 'rgb(13 148 136)',
-  loading: false,
-})
+}
+
+const { size = 'small', type = 'primary', color = 'rgb(13 148 136)', icon, loading = false } = defineProps<MyButtonProps>()
 </script>
 
 <template>
-  <el-button :type="type" :color="color" :size="size" :loading="loading">
+  <el-button v-bind="{ ...$attrs, ...$props }">
     <div fc gap-4px>
       <Icon v-if="icon && !loading" :icon="icon" />
       <slot />
