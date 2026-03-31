7bcbabc0cc6df9df5438b31e65f0670dbfd36bb4
Waaatanuki <watanukipublic@gmail.com>
2026-03-31 11:21:50 +0800

    feat: 更新lint规则

--- eslint.config.js
+++ eslint.config.js
@@ -2,15 +2,13 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   unocss: true,
-  formatters: {
-    css: true,
-    html: true,
-  },
+  formatters: true,
   rules: {
     'no-unused-vars': 'warn',
     'unused-imports/no-unused-vars': 'warn',
     'node/prefer-global/process': 'off',
     'pnpm/yaml-enforce-settings': 'off',
     'pnpm/json-enforce-catalog': 'off',
+    'e18e/prefer-static-regex': 'off',
   },
 })
